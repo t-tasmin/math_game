91cfcaca8daf96424cbe5090f21b04b0b8117a3b
Tania Tasmin <taniat.kaau@email.com>
2022-01-03 14:36:43 -0500

    Start a new game

--- game.rb
+++ game.rb
@@ -1,11 +1,33 @@
+require './players'
+require './question'
+
 class Game
 
-  def initialize
-    @current_player = ""
+ 
+  def start
+    puts 'Start Game'
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+    turn
   end
 
-  def start
-    puts 'start game'
-  end
+  def turn
+    question=Question.new
+    correct = question.generate(@player1)
+    user = gets.chomp.to_i
+    question.verify(user, @player1)
+    
+    if @player1.lives == 0
+      puts 'player 2 wins'
+      puts '----- GAME OVER ----'
+    else
+      puts '----- NEW TURN ----'
+    end  
+    
+  end  
+    
+
+  
+  
 
  end
