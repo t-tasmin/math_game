12fc623722882c5d5c9f1418f2e400da104534f0
Tania Tasmin <taniat.kaau@email.com>
2022-01-03 15:36:50 -0500

    Refactor to reuse codes

--- game.rb
+++ game.rb
@@ -10,36 +10,42 @@ class Game
     turn
   end
 
-  def turn
+  def generate_verify
     question=Question.new
-    correct = question.generate(@player1)
+    question.generate(@player)
     user = gets.chomp.to_i
-    question.verify(user, @player1)
-    puts "P1: #{@player1.lives}/3  vs. P2: #{@player2.lives}/3"
-    
-    if @player1.lives == 0
-      puts "Player 2 wins with a score of #{player2.lives}/3"
-      puts '----- GAME OVER ----'
-    else
-      puts '----- NEW TURN ----'
-      question=Question.new
-      correct = question.generate(@player2)
-      user = gets.chomp.to_i
-      question.verify(user, @player2)
-      puts "P1: #{@player1.lives}/3  vs. P2: #{@player2.lives}/3"
-        if @player2.lives == 0
-          puts "Player 1 wins with a score of #{@player1.lives}/3"
-          puts '----- GAME OVER ----'
-        else
-          puts '----- NEW TURN ----'
-          turn
-        end  
-    end  
-    
+    question.verify(user, @player)
   end  
+
+  def game_status
+    puts "P1: #{@player1.lives}/3  vs. P2: #{@player2.lives}/3"
+  end  
+
+  def win_message
+    if @player.lives == 0
+      puts "#{@other.name} wins with a score of #{@other.lives}/3"
+      puts '----- GAME OVER ----'
+      exit(0)
+    end
+  end
+
+  def turn
+    @player = @player1
+    @other = @player2
+    generate_verify
+    game_status
+    win_message
     
+    puts '----- NEW TURN ----'
+    @player = @player2
+    @other = @player1
+    generate_verify
+    game_status
+    win_message
 
-  
-  
-
+    puts '----- NEW TURN ----'
+    turn
+ 
+  end  
+ 
  end
