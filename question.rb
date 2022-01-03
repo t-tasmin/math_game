f05adb7ce2e4a7d620d99832384522e2da027b20
Tania Tasmin <taniat.kaau@email.com>
2022-01-03 14:39:03 -0500

    Add verify method to verify answer

--- question.rb
+++ question.rb
@@ -1,10 +1,22 @@
 class Question
 
-  def generate
+  def generate(player)
     num1=rand(1..10)
     num2=rand(1..10)
-    puts "What does #{num1} plus #{num2} equal?"
-    answer = num1+num2
+    puts "#{player.name}: What does #{num1} plus #{num2} equal?"
+    @correct = num1 + num2
+    return @correct
   end  
+
+  
+  def verify(user, player)
+    if (user == @correct)
+      puts "#{player.name}: Yes! You are Correct"
+    else
+      puts "#{player.name}: Seriously? No!"
+      player.lives_lose
+    end  
+  end
+
 
 end
